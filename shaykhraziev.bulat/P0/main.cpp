f5dda32c9b8f3ee4e11004c63447f43d781e399e
CWSHBR <mrcookie209@gmail.com>
2025-10-22 21:14:45 +0300

    formatted using cg

--- shaykhraziev.bulat/P0/main.cpp
+++ shaykhraziev.bulat/P0/main.cpp
@@ -1,8 +1,9 @@
-//
 // Created by Bulat Shaykhraziev on 22.10.2025.
-//
+
 #include <iostream>
 
-int main() {
-    std::cout << "shaykhraziev.bulat" << std::endl;
+int main()
+{
+  std::cout << "shaykhraziev.bulat" << std::endl;
+  return 0;
 }
