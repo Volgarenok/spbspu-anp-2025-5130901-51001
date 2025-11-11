d44972643e393b1c5c34921e966aa29fba9ed43d
Kreniker <kitserov.mike@gmail.com>
2025-11-11 12:14:52 +0300

    Input errors

--- kitserov.mihail/P3/main.cpp
+++ kitserov.mihail/P3/main.cpp
@@ -1,2 +1,25 @@
-int main()
-{}
+#include <iostream>
+int main(int argc, char* argv[])
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enought arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  int num = atoi(argv[1]);
+  char* input = argv[2];
+  char* output = argv[3];
+  if (num != 1 and num != 2)
+  {
+    std::cerr << "First argument incorrect\n";
+    return 1;
+  }
+  std::cout << num << std::endl;
+  std::cout << input << std::endl;
+  std::cout << output << std::endl;
+}
