fd7f7cb3a49917eae63106100f89421a156a6c0a
L0syash <losev_timofey07@mail.ru>
2025-11-11 23:52:49 +0300

    Create reader first file(for input) and connectr with creater arrays

--- losev.timofey/P3/main.cpp
+++ losev.timofey/P3/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char **argv) {
-  int n = 2, m = 2;
+  std::ifstream input(argv[2]);
+  int m, n;
+  if(input.is_open()){
+    input >> m >> n;
+  }
   if (*argv[1] == '1'){
     int arrayStatic[10000];
     std:: cout << "I create static array\n";
   }
   else if (*argv[1] == '2'){
     int *arrayDinamic = new int[n*m];
-    std:: cout << "I create dinamic array\n";
+    std:: cout << "I create dinamic array\n His size: " << n*m <<std::endl;
   }
+  else{
+    std::cout << "Error wrong num";
+    return 1;
+  }
+  return 0;
 }
