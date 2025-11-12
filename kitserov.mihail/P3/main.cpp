c83feb6639b9ee13dc91f61978b242e0435e9f34
Kreniker <kitserov.mike@gmail.com>
2025-11-12 17:14:37 +0300

    First variant. On future: simpler massive :(

--- kitserov.mihail/P3/main.cpp
+++ kitserov.mihail/P3/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <fstream>
+void variant_1(size_t rows, size_t cols, int matrix[][100], const char* output_file)
+{
+	size_t answer = 0;
+	for (size_t i = 0; i < rows; i++)
+	{
+		bool flag = false;
+		for (size_t j = 0; j < (cols-1); j++)
+		{
+			if (matrix[i][j] == matrix[i][j+1])
+			{
+				flag = true;
+				answer++;
+				break;
+			}
+	    }
+	}
+	std::ofstream output(output_file);
+	output << answer;
+	output.close();
+}
+
 int main(int argc, char* argv[])
 {
   if (argc < 4)
@@ -13,7 +34,6 @@ int main(int argc, char* argv[])
     return 1;
   }
   int num = atoi(argv[1]);
-  char* output = argv[3];
   if (num != 1 and num != 2)
   {
     std::cerr << "First argument incorrect\n";
@@ -25,11 +45,35 @@ int main(int argc, char* argv[])
     std::cerr << "Failed open input file\n";
     return 2;
   }
-  int rows, cols;
+  size_t rows, cols;
   input >> rows >> cols;
   if (input.fail())
   {
-    std::cerr << "Failed read matrix\n";
+    std::cerr << "Failed read matrix dimentions\n";
     return 2;
   }
+  if (rows < 0 or cols < 0)
+  {
+    std::cerr << "Incorrect matrix dimensions\n";
+  }
+  if (num == 1)
+  {
+    //int matrix[rows][cols];
+    int matrix[100][100];
+    for (size_t i; i < rows; i++)
+    {
+      for (size_t j; j < cols; j++)
+      {
+        input >> matrix[i][j];
+        if (input.fail())
+        {
+          std::cerr << "Failed read matrix's element\n";
+          return 2;
+        }
+      }
+    }
+    variant_1(rows, cols, matrix, argv[3]);
+  }
+
+  return 0;
 }
