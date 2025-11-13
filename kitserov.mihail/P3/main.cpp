39d02e73a1e29d3eda0c107cca04c83d20f5ade4
Kreniker <kitserov.mike@gmail.com>
2025-11-13 14:42:05 +0300

    Second variant with dinamic duration

--- kitserov.mihail/P3/main.cpp
+++ kitserov.mihail/P3/main.cpp
@@ -1,24 +1,66 @@
 #include <iostream>
 #include <fstream>
-void variant_1(size_t rows, size_t cols, int matrix[][100], const char* output_file)
+int* create_matrix(size_t size) {
+    if (size == 0) {
+        return nullptr;
+    }
+    return new int[size];
+}
+
+void variant_1(size_t rows, size_t cols, int matrix[10000], const char* output_file)
 {
 	size_t answer = 0;
+	bool flag;
 	for (size_t i = 0; i < rows; i++)
 	{
-		bool flag = false;
+		flag = false;
 		for (size_t j = 0; j < (cols-1); j++)
 		{
-			if (matrix[i][j] == matrix[i][j+1])
+			if (matrix[i * cols + j] == matrix[i * cols + j + 1])
 			{
 				flag = true;
-				answer++;
 				break;
 			}
+	    }
+	    if (!flag)
+	    {
+	    	answer++;
 	    }
 	}
 	std::ofstream output(output_file);
 	output << answer;
-	output.close();
+}
+
+void variant_2(size_t rows, size_t cols, int* matrix, const char* output_file)
+{
+	std::ofstream output(output_file);
+	if (rows != cols or rows == 0 or cols == 0)
+	{
+		output << "false";
+		delete[] matrix;
+		return;
+	}
+	bool flag = true;
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < i; j++) {  // только элементы ниже диагонали (j < i)
+            if (matrix[i * cols + j] != 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag) {
+            break;
+        }
+    }
+	if (flag)
+	{
+		output << "true";
+	}
+	else
+	{
+		output << "false";
+	}
+	delete[] matrix;
 }
 
 int main(int argc, char* argv[])
@@ -58,22 +100,40 @@ int main(int argc, char* argv[])
   }
   if (num == 1)
   {
-    //int matrix[rows][cols];
-    int matrix[100][100];
-    for (size_t i; i < rows; i++)
+    int matrix[10000];
+    for (size_t i; i < rows*cols; i++)
     {
-      for (size_t j; j < cols; j++)
-      {
-        input >> matrix[i][j];
-        if (input.fail())
-        {
-          std::cerr << "Failed read matrix's element\n";
-          return 2;
-        }
-      }
+	     input >> matrix[i];
+	     if (input.fail())
+	     {
+	       std::cerr << "Failed read matrix's element\n";
+	       return 2;
+	     }
     }
     variant_1(rows, cols, matrix, argv[3]);
   }
 
+  else
+  {
+    int*matrix = nullptr;
+	matrix = create_matrix(rows*cols);
+	if (matrix == nullptr)
+	{
+		std::cerr << "Failed to allocate memory for matrix\n";
+		return 2;
+	}
+    for (size_t i; i < rows*cols; i++)
+    {
+	     input >> matrix[i];
+	     if (input.fail())
+	     {
+	       std::cerr << "Failed read matrix's element\n";
+	       delete[] matrix;
+	       return 2;
+	     }
+    }
+  	variant_2(rows, cols, matrix, argv[3]);
+  }
+  std::cout << "Succesful\n";
   return 0;
 }
