423bbaa574e70168b8d8791edcdbb7fe8fc87a2b
L0syash <losev_timofey07@mail.ru>
2025-11-14 00:10:43 +0300

    created the basic functions, add output answer, removed the copypasta

--- losev.timofey/P3/main.cpp
+++ losev.timofey/P3/main.cpp
@@ -1,82 +1,84 @@
 #include <iostream>
 #include <fstream>
-#include <limits> 
+#include <limits>
 
-namespace losev{
+namespace losev {
   void readFile(std::ifstream & file, int * ptrArr, size_t m, size_t n);
+  int numRowLSR(int * ptrArr, int m, int n);
+  int countLocMin(int * ptrArr, size_t m, size_t n);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
   namespace los = losev;
-  if (argc > 4){
+  if (argc > 4) {
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  else if(argc < 4){
+  else if (argc < 4) {
     std::cerr << "Too few arguments\n";
+    return 1;
+  }
+  std::ofstream output(argv[3]);
+  if (!output.is_open()) {
+    std::cerr <<"I can not open output file\n";
+    return 1;
+  }
+  else {
+    output.clear();
   }
   std::ifstream input(argv[2]);
   int m = -1, n = -1;
-  if(input.is_open()){
+  if (input.is_open()) {
     input >> m >> n;
     if (input.fail()) {
         std::cerr << "I can not read matrix size\n";
         return 2;
     }
-    if (m < 0 || n < 0){
+    if (m < 0 || n < 0) {
       std::cerr << "I can not create an array from this\n";
       return 2;
     }
+    else if (m == 0 && n == 0) {
+      output << 0 << std::endl << 0 << " " << 0 << std::endl;
+      return 0;
+    }
   }
-  else{
-    std::cerr <<"I can not open file\n";
+  else {
+    std::cerr <<"I can not open input file\n";
     return 1;
   }
   int *arrayDinamic = nullptr;
   int arrayStack[10000];
-  if (*argv[1] == '1'){
-    std::cout << "I create static array\n";
-    try{
-      los::readFile(input, arrayStack, m, n);
-    }
-    catch(const std::runtime_error& e) {
-        std::cerr << "Error file: " << e.what() << std::endl;
-        return 2;
-    }
-    for(size_t i = 0; i < m; i++){
-      for(size_t j = 0; j < n; j++){
-        std::cout << arrayStack[i * n + j] << " ";
-      }
-    }
+  int *array = nullptr;
+  if (*argv[1] == '1') {
+    array = arrayStack;
   }
-  else if (*argv[1] == '2'){
+  else if (*argv[1] == '2') {
     arrayDinamic = new int[n * m];
-    try{
-      los::readFile(input, arrayDinamic, m, n);
-    }
-    catch(const std::runtime_error& e) {
-        std::cerr << "Error file: " << e.what() << std::endl;
-        return 2;
-    }
-    std::cout << "I create dinamic array\n His size: " << n * m <<std::endl;
-    for(size_t i = 0; i < m; i++){
-      for(size_t j = 0; j < n; j++){
-        std::cout << arrayDinamic[i * n + j] << " ";
-      }
-    }
-    
+    array = arrayDinamic;
   }
-  else{
+  else {
     std::cerr << "Error wrong num\n";
     return 1;
   }
-
+  try {
+    los::readFile(input, array, m, n);
+  }
+  catch (const std::runtime_error& e) {
+    std::cerr << "Error file: " << e.what() << std::endl;
+    return 2;
+  }
+  input.close();
+  output << los::countLocMin(array, m, n) << std::endl;
+  output << los::numRowLSR(array, m, n) << std::endl;
+  delete[] arrayDinamic;
   return 0;
 }
-void losev::readFile(std::ifstream & file, int * ptrArr, size_t m, size_t n){
+
+void losev::readFile(std::ifstream & file, int * ptrArr, size_t m, size_t n) {
   long long temp;
-  for(size_t i = 0; i < m; i++){
-    for(size_t j = 0; j < n; j++){
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < n; j++) {
       file >> temp;
       if (file.fail()) {
         throw std::runtime_error("I can not read matrix");
@@ -87,4 +89,50 @@ void losev::readFile(std::ifstream & file, int * ptrArr, size_t m, size_t n){
       ptrArr[i * n + j] = (int)temp;
     }
   }
+}
+
+int losev::countLocMin(int * ptrArr, size_t m, size_t n) {
+  int count = 0;
+  bool isLocMin;
+  for (size_t i = 1; i < m - 1; i++) {
+    for (size_t j = 1; j < n - 1; j++) {
+      isLocMin = ptrArr[i * n + j] < ptrArr[i * n + (j + 1)] &&
+                  ptrArr[i * n + j] < ptrArr[(i + 1) * n + (j + 1)] &&
+                  ptrArr[i * n + j] < ptrArr[(i + 1) * n + j] &&
+                  ptrArr[i * n + j] < ptrArr[(i + 1) * n + (j - 1)] &&
+                  ptrArr[i * n + j] < ptrArr[i * n + (j - 1)] &&
+                  ptrArr[i * n + j] < ptrArr[(i - 1) * n + (j - 1)] &&
+                  ptrArr[i * n + j] < ptrArr[(i - 1) * n + j] &&
+                  ptrArr[i * n + j] < ptrArr[(i - 1) * n + (j + 1)];
+      if (isLocMin) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+int losev::numRowLSR(int * ptrArr, int m, int n) {
+  int numRowMaxsLenRep = 0;
+  int maxLenRep = 0;
+  for (int i = 0; i < m; i++) {
+    int locMaxLenRep = 0;
+    int lenRep = 0;
+    for (int j = 0; j < n - 1; j++) {
+      if (ptrArr[i * n + j] == ptrArr[i * n + j + 1]) {
+        lenRep++;
+        if (lenRep > locMaxLenRep) {
+          locMaxLenRep = lenRep;
+        }
+      }
+      else {
+        lenRep = 0;
+      }
+      if (locMaxLenRep > maxLenRep) {
+        numRowMaxsLenRep = i;
+        maxLenRep = locMaxLenRep;
+      }
+    }
+  }
+  return numRowMaxsLenRep + 1;
 }
