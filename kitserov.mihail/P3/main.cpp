d174a6dcb3d3582663326c805f04eb6da000d6a1
Kreniker <kitserov.mike@gmail.com>
2025-11-14 12:47:02 +0300

    CG second try

--- kitserov.mihail/P3/main.cpp
+++ kitserov.mihail/P3/main.cpp
@@ -20,7 +20,7 @@ void kitserov::variant1(size_t rows, size_t cols, int matrix[10000], const char*
 {
   size_t answer = 0;
   bool flag = false;
-  
+
   for (size_t i = 0; i < rows; i++) {
     flag = false;
     for (size_t j = 0; j < (cols - 1); j++) {
@@ -33,7 +33,7 @@ void kitserov::variant1(size_t rows, size_t cols, int matrix[10000], const char*
       answer++;
     }
   }
-  
+
   std::ofstream output(outputFile);
   output << answer;
 }
@@ -46,7 +46,7 @@ void kitserov::variant2(size_t rows, size_t cols, int* matrix, const char* outpu
     delete[] matrix;
     return;
   }
-  
+
   bool flag = true;
   for (size_t i = 0; i < rows; i++) {
     for (size_t j = 0; j < i; j++) {
@@ -59,7 +59,7 @@ void kitserov::variant2(size_t rows, size_t cols, int* matrix, const char* outpu
       break;
     }
   }
-  
+
   if (flag) {
     output << "true";
   } else {
@@ -71,43 +71,43 @@ void kitserov::variant2(size_t rows, size_t cols, int* matrix, const char* outpu
 int main(int argc, char* argv[])
 {
   using namespace kitserov;
-  
+
   if (argc < 4) {
     std::cerr << "Not enough arguments\n";
     return 1;
   }
-  
+
   if (argc > 4) {
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  
+
   int num = std::atoi(argv[1]);
   if (num != 1 && num != 2) {
     std::cerr << "First argument incorrect\n";
     return 1;
   }
-  
+
   std::ifstream input(argv[2]);
   if (!input.is_open()) {
     std::cerr << "Failed to open input file\n";
     return 2;
   }
-  
+
   size_t rows = 0;
   size_t cols = 0;
   input >> rows >> cols;
-  
+
   if (input.fail()) {
     std::cerr << "Failed to read matrix dimensions\n";
     return 2;
   }
-  
+
   if (rows == 0 || cols == 0) {
     std::cerr << "Incorrect matrix dimensions\n";
     return 2;
   }
-  
+
   if (num == 1) {
     int matrix[10000] = {0};
     for (size_t i = 0; i < rows * cols; i++) {
@@ -124,7 +124,7 @@ int main(int argc, char* argv[])
       std::cerr << "Failed to allocate memory for matrix\n";
       return 2;
     }
-    
+
     for (size_t i = 0; i < rows * cols; i++) {
       input >> matrix[i];
       if (input.fail()) {
@@ -135,6 +135,6 @@ int main(int argc, char* argv[])
     }
     variant2(rows, cols, matrix, argv[3]);
   }
-  
+
   return 0;
 }
