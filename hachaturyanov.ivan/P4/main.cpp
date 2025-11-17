bc8942efb07009f88e7eb0c6ba684da5f7cb45fe
inshady <inshadyprod@gmail.com>
2025-11-17 22:08:42 +0300

    input with increasing buffer and draft EXC-SND

--- hachaturyanov.ivan/P4/main.cpp
+++ hachaturyanov.ivan/P4/main.cpp
@@ -1,24 +1,67 @@
 #include <iostream>
 #include <cctype>
 #include <cstdlib>
+#include <cstring>
 
 namespace hachaturyanov {
-  size_t max_buffer_size = 1024;
+  char * readline(char * buf, size_t * buf_size);
+  void exc_snd(char * input, char * second, size_t second_len, char * output);
+}
+
+char * hachaturyanov::readline(char * buf, size_t * buf_size) {
+  size_t len = 0;
+
+  bool done = false;
+  while (!done) {
+    std::cin.getline(buf + len, *buf_size - len);
+    len = strlen(buf);
+
+    if (std::cin.fail() && !std::cin.eof()) {
+      std::cin.clear();
+      *buf_size *= 2;
+      char * temp = (char*)realloc(buf, *buf_size);
+      if (!temp) {
+        free(buf);
+        return nullptr;
+      }
+      buf = temp;
+      continue;
+    }
+    done = true;
+  }
+  return buf;
+}
+
+void hachaturyanov::exc_snd(char * input, char * second, size_t second_len, char * output) {
+  for (size_t i = 0; i < strlen(input); i++) {
+    size_t count = 0;
+    for (size_t j = 0; j < second_len; j++) {
+      if (input[i]==second[j]) {
+        break;
+      }
+    }
+    output[i] = input[i];
+  }
 }
 
 int main() {
-  const char * exc_snd_second = "abc";
-  const char * dgt_snd_second = "g1h2k";
+  size_t max_buffer_size = 512;
 
-  char * input = (char*)malloc(hachaturyanov::max_buffer_size);
+  const char exc_snd_second[] = "abc";
+  const char dgt_snd_second[] = "g1h2k";
+
+  size_t exc_snd_s_len = strlen(exc_snd_second);
+  size_t dgt_snd_s_len = strlen(dgt_snd_second);
+
+  char * input = (char*)malloc(max_buffer_size);
   if (!input) {
     std::cerr << "Bad allocation\n";
     return 1;
   }
 
-  char * output = (char*)malloc(hachaturyanov::max_buffer_size);
-  if (!output) {
-    std::cerr << "Bad allocation\n";
+  input = hachaturyanov::readline(input, &max_buffer_size);
+  if (!input) {
+    std::cerr << "Too long input\n";
     return 1;
   }
 
