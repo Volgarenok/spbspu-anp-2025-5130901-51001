a5220a265f896a94622171e2f6ab0f6dab41af0a
L0syash <losev_timofey07@mail.ru>
2025-11-19 23:43:21 +0300

    make function getline with malloc

--- losev.timofey/P4/main.cpp
+++ losev.timofey/P4/main.cpp
@@ -1,17 +1,52 @@
 #include<iostream>
 #include<iomanip>
+#include<stdlib.h>
 
 namespace losev {
   size_t getline(std::istream & in, char * data, size_t size);
+  char * getline(std::istream & in, size_t & size);
 }
 
 
 int main() {
   namespace los = losev;
-  char str[] = "!!!!!";
-  size_t str_size = 5;
-  size_t k = los::getline(std::cin, str, str_size);
+  size_t str_size = 10;
+  char * str =  los::getline(std::cin, str_size);
   std::cout << str << "\n";
+}
+
+char * losev::getline(std::istream & in, size_t & size) {
+  char * str = reinterpret_cast<char*>(malloc(size * sizeof(char)));
+  if (!str) {
+    std::cerr << "Memory allocation failed\n";
+    free(str);
+    return nullptr;
+  }
+  bool is_skips = in.flags() & std::ios_base::skipws;
+  if (is_skips) {
+    in >> std::noskipws;
+  }
+  for (size_t i = 0; in && i < size; ++i) {
+    in >> str[i];
+    if (i == (size - 1) && str[i] != 0) {
+      char * str_new = reinterpret_cast<char*>(malloc((size + 10) * sizeof(char)));
+      if (!str_new) {
+        std::cerr << "Memory allocation failed\n";
+        free(str_new);
+        free(str);
+        return nullptr;
+      }
+      for (size_t j = 0; j < size; j++) {
+        str_new[j] = str[j];
+      }
+      size += 10;
+      str = str_new;
+    }
+  }
+  if (is_skips) {
+    in >> std::skipws;
+  }
+  return str;
 }
 
 size_t losev::getline(std::istream & in, char * data, size_t size) {
@@ -20,7 +55,7 @@ size_t losev::getline(std::istream & in, char * data, size_t size) {
     in >> std::noskipws;
   }
   size_t i = 0;
-  for(; in && i < size; ++i) {
+  for (; in && i < size; ++i) {
     in >> data[i];
   }
   if (is_skips) {
