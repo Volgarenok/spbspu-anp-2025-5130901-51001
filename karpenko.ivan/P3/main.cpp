b6f1fbcc13df0066c4789aa23c4d019b3d52cd0f
restorte <vanyakarpenko837@yandex.ru>
2025-11-29 22:10:45 +0300

    fix CG

--- karpenko.ivan/P3/main.cpp
+++ karpenko.ivan/P3/main.cpp
@@ -12,8 +12,9 @@ namespace karpenko
   void transformMatrixSpiral(std::size_t rows, std::size_t cols, int matrix[])
   {
     if (rows == 0 || cols == 0)
+    {
       return;
-
+    }
     std::size_t top = 0, bottom = rows - 1;
     std::size_t left = 0, right = cols - 1;
     int counter = 1;
@@ -21,24 +22,32 @@ namespace karpenko
     while (top <= bottom && left <= right)
     {
       for (std::size_t i = top; i <= bottom; i++)
+      {
         matrix[i * cols + left] += counter++;
+      }
       left++;
 
       for (std::size_t i = left; i <= right; i++)
+      {
         matrix[bottom * cols + i] += counter++;
+      }
       bottom--;
 
       if (left <= right)
       {
         for (std::size_t i = bottom + 1; i > top; i--)
+        {
           matrix[(i - 1) * cols + right] += counter++;
+        }
         right--;
       }
 
       if (top <= bottom)
       {
         for (std::size_t i = right + 1; i > left; i--)
+        {
           matrix[top * cols + (i - 1)] += counter++;
+        }
         top++;
       }
     }
@@ -47,8 +56,9 @@ namespace karpenko
   void createSmoothedMatrix(std::size_t rows, std::size_t cols, int matrix[], double smoothed[])
   {
     if (rows == 0 || cols == 0)
+    {
       return;
-
+    }
     for (std::size_t i = 0; i < rows; i++)
     {
       for (std::size_t j = 0; j < cols; j++)
@@ -61,12 +71,13 @@ namespace karpenko
           for (int dj = -1; dj <= 1; dj++)
           {
             if (di == 0 && dj == 0)
+            {
               continue;
-
-            if ((di == -1 && i == 0) || (di == 1 && i == rows - 1) ||
-                (dj == -1 && j == 0) || (dj == 1 && j == cols - 1))
+            }
+            if ((di == -1 && i == 0) || (di == 1 && i == rows - 1) || (dj == -1 && j == 0) || (dj == 1 && j == cols - 1))
+            {
               continue;
-
+            }
             std::size_t ni = i + di;
             std::size_t nj = j + dj;
             sum += matrix[ni * cols + nj];
