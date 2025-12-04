ce71e2a194a35141dd759876cf95d39ea4fbc8a9
yuklya <yuloseva@mail.ru>
2025-12-04 14:02:16 +0300

    fix main

--- loseva.yulia/P3/main.cpp
+++ loseva.yulia/P3/main.cpp
@@ -71,69 +71,71 @@ void loseva::countLocalMax(size_t rows, size_t cols, int** matrix)
 
 int main(int argc, char* argv[])
 {
-    if (argc < 4) {
-        std::cerr << "Not enough arguments\n";
-        return 1;
-    }
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
 
-    if (argc > 4) {
-        std::cerr << "Too many arguments\n";
-        return 1;
-    }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
 
-    char* end;
-    long num = std::strtol(argv[1], &end, 10);
-    if (*end != '\0' ||  num < 1 ||  num > 2) {
-        std::cerr << "First parameter is incorrect\n";
-        return 1;
-    }
+  char* end;
+  long num = std::strtol(argv[1], &end, 10);
+  if (*end != '\0' ||  num < 1 ||  num > 2)
+  {
+    std::cerr << "First parameter is incorrect\n";
+    return 1;
+  }
 
-    std::ifstream input(argv[2]);
-    if (!input.is_open()) {
-        std::cerr << "Cannot open input file\n";
+  std::ifstream input(argv[2]);
+  if (!input.is_open()) 
+  {
+    std::cerr << "Cannot open input file\n";
+    return 2;
+  }
+
+  size_t rows, cols;
+  input >> rows >> cols;
+  if (input.fail())
+  {
+    std::cerr << "Cannot read matrix dimensions\n";
+    return 2;
+  }
+  int* matrix = nullptr;
+
+  if (rows == 0 || cols == 0)
+  {
+    matrix = new int[0];
+  }
+  else 
+  {
+    matrix = new int[rows * cols];
+    for (size_t i = 0; i < rows * cols; i++) 
+    {
+      input >> matrix[i];
+      if (input.fail())
+      {
+        std::cerr << "Cannot read matrix element\n";
+        delete[] matrix;
         return 2;
+      }
     }
+    size_t result = 0;
+    if (num == 1)
+    { 
+      result = loseva::maxDiagonalSum(rows, cols, matrix);
+    }
+    else
+    {
+      result = countLocalMax(rows, cols, matrix);
+    }
+    std::ofstream output(argv[3]);
+    output << result;
 
-    size_t rows, cols;
-    input >> rows >> cols;
-    if (input.fail()) {
-        std::cerr << "Cannot read matrix dimensions\n";
-        return 2;
-    }
-
-    if (rows == 0 || cols == 0) {
-        std::ofstream output(argv[3]);
-        output << "0 0\n0\n";
-        return 0;
-    }
-
-    if ((num == 1)) {
-        if (rows * cols > 10000) {
-            std::cerr << "Matrix too large for fixed array\n";
-            return 2;
-        }
-        int matrix[10000];
-        for (size_t i = 0; i < rows * cols; i++) {
-            input >> matrix[i];
-            if (input.fail()) {
-                std::cerr << "Cannot read matrix element\n";
-                return 2;
-            }
-        }
-        loseva::elementsdiag(rows, cols, matrix, argv[3]);
-    }
-    else {
-        int* matrix = new int[rows * cols];
-        for (size_t i = 0; i < rows * cols; i++) {
-            input >> matrix[i];
-            if (input.fail()) {
-                std::cerr << "Cannot read matrix element\n";
-                delete[] matrix;
-                return 2;
-            }
-        }
-        loseva::localmax(rows, cols, matrix, argv[3]);
-    }
-
+    delete[] matrix;
     return 0;
 }
