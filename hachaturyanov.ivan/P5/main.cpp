60e1ca2aa70105ad0a7cf60001aa4961c764ae0c
inshady <inshadyprod@gmail.com>
2025-12-10 04:32:09 +0300

    defined Shape

--- hachaturyanov.ivan/P5/main.cpp
+++ hachaturyanov.ivan/P5/main.cpp
@@ -9,6 +9,15 @@ namespace hachaturyanov {
     size_t width, height;
     int pos;
   };
+
+  struct Shape {
+    virtual size_t getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t pnt) = 0;
+    virtual void move(int xsh, int ysh) = 0;
+    virtual void scale(double k) = 0;
+    virtual ~Shape() = default;
+  };
 }
 
 int main()
