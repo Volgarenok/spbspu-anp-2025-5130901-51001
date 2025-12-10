b51c75cb08303d5393d694f56d586d6963c6185f
Kreniker <kitserov.mike@gmail.com>
2025-12-10 16:30:06 +0300

    POD and base struct Shape

--- kitserov.mihail/P5/main.cpp
+++ kitserov.mihail/P5/main.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+struct point_t
+{
+  int x, y;
+};
+
+struct rectangle_t
+{
+  int width, height;
+  point_t pos;
+};
+
+struct Shape
+{
+  virtual size_t getArea() const;
+  virtual rectangle_t getFrameRect() const;
+  virtual void move(int dx, int dy);
+  virtual void move(point_t p);
+  virtual void scale(int k);
+};
+
 int main()
-{}
+{
+  
+}
