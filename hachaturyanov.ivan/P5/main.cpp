c72b60a042feeb5ec9110288d094637d10c24810
inshady <inshadyprod@gmail.com>
2025-12-10 04:10:41 +0300

    POD-types

--- hachaturyanov.ivan/P5/main.cpp
+++ hachaturyanov.ivan/P5/main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+
+namespace hachaturyanov {
+  struct point_t {
+    int x, y;
+  };
+
+  struct rectangle_t {
+    size_t width, height;
+    int pos;
+  };
+}
+
 int main()
 {
 
