1951014038bbeb2ec588740771abe999322de7da
inshady <inshadyprod@gmail.com>
2025-12-10 07:07:11 +0300

    edited definiton of Complexquad and implted few mthds

--- hachaturyanov.ivan/P5/main.cpp
+++ hachaturyanov.ivan/P5/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 namespace hachaturyanov {
   struct point_t {
@@ -32,21 +33,51 @@ namespace hachaturyanov {
   };
 
   struct Complexquad: Shape {
-    explicit Complexquad(double sa, double sb, point_t p);
+    explicit Complexquad(double d1, double d2, point_t p);
     double getArea() const override;
     rectangle_t getFrameRect() const override;
     void move(point_t pnt) override;
     void move(double xsh, double ysh) override;
     void scale(double k) override;
    private:
-    double sidea, sideb;
+    point_t vertices[4];
+    double diag1, diag2;
     point_t pos;
   };
+
+  double triangleArea(const point_t v1, const point_t v2, const point_t v3);
 }
 
 int main()
 {
 
+}
+
+double hachaturyanov::triangleArea(const point_t v1, const point_t v2, const point_t v3) {
+  return 0.5 * std::abs(v1.x * (v2.y - v3.y) + v2.x * (v3.y - v1.y) + v3.x * (v1.y - v2.y));
+}
+
+hachaturyanov::Complexquad::Complexquad(double d1, double d2, point_t p):
+ Shape(),
+ vertices({}),
+ diag1(d1),
+ diag2(d2),
+ pos(p)
+{
+  if (d1 > 0 && d2 > 0) {
+    vertices[0] = {p.x - d1 / 2, p.y};
+    vertices[1] = {p.x + d1 / 2, p.y};
+    vertices[2] = {p.x, p.y - d2 / 2};
+    vertices[3] = {p.x, p.y + d2 / 2};
+  } else {
+    throw std::logic_error("diagonals must be positive");
+  }
+}
+
+double hachaturyanov::Complexquad::getArea() const {
+  double area1 = hachaturyanov::triangleArea(pos, vertices[1], vertices[2]);
+  double area2 = hachaturyanov::triangleArea(pos, vertices[0], vertices[3]);
+  return area1 + area2;
 }
 
 hachaturyanov::Rectangle::Rectangle(double w, double h, point_t p):
