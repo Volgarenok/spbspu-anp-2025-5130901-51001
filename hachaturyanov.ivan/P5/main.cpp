7b0dceca1077b43034d51eea1bc399e0443f9430
inshady <inshadyprod@gmail.com>
2025-12-10 04:46:24 +0300

    defined Rectangle

--- hachaturyanov.ivan/P5/main.cpp
+++ hachaturyanov.ivan/P5/main.cpp
@@ -11,7 +11,7 @@ namespace hachaturyanov {
   };
 
   struct Shape {
-    virtual size_t getArea() const = 0;
+    virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(point_t pnt) = 0;
     virtual void move(double xsh, double ysh) = 0;
@@ -19,7 +19,17 @@ namespace hachaturyanov {
     virtual ~Shape() = default;
   };
 
-
+  struct Rectangle: Shape {
+    explicit Rectangle(double width, double height, point_t pos);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t pnt) override;
+    void move(double xsh, double ysh) override;
+    void scale(double k) override;
+   private:
+    double width, height;
+    point_t pos;
+  };
 }
 
 int main()
