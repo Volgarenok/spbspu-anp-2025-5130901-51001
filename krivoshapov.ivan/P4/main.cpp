dab69f29aded88d87fc6ccb78d5030ebd2af20f0
ahahhaha67 <krivoshapovewan@yandex.ru>
2025-12-11 22:14:37 +0300

    add vowel detection

--- krivoshapov.ivan/P4/main.cpp
+++ krivoshapov.ivan/P4/main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
+namespace krivoshapov {
+    size_t rmVow (const char* src, char* dst, size_t dstSz){
+        if (src == nullptr || dst == nullptr || dstSz == 0){
+            return 0;
+        }
+
+        const char vowels [] = "eiouyAEIOUY";
+        const size_t vowelCnt = 12;
+        size_t writeIdx = 0;
+        size_t srcLen = std::strlen(src);
+    }
+
+}
 
 int main(){
     
