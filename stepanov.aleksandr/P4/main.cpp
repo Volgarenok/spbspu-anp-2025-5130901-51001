5fb982c75f2cc0f40d7ed0df651beda32d7e3e2c
btless <zovitemenakrutoj@gmail.com>
2025-12-12 06:50:18 +0300

    fix

--- stepanov.aleksandr/P4/main.cpp
+++ stepanov.aleksandr/P4/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
 namespace stepanov {
-  char* getLine(std::istream& in);
+  char* getLine(std::istream& in, size_t& size);
   void toLower(char* str);
-  void removeVowels(char* str);
+  void removeVowels(const char* str, char* output);
   bool isVowel(const char c);
 }
 
 int main()
 {
-  char* str = stepanov::getLine(std::cin);
-  if (str == nullptr) {
+  size_t size = 0;
+  char* str = nullptr;
+  try {
+    str = stepanov::getLine(std::cin, size);
+  } catch (const std::bad_alloc& e) {
     std::cerr << "Cannot read input\n";
     return 1;
   }
@@ -21,64 +24,59 @@ int main()
   }
 
   stepanov::toLower(str);
-  std::cout << str << std::endl;
-  stepanov::removeVowels(str);
-  std::cout << str << std::endl;
+  std::cout << str << '\n';
+
+  char *withoutVowels = new char[size];
+  stepanov::removeVowels(str, withoutVowels);
+  std::cout << withoutVowels << '\n';
 
   delete[] str;
   return 0;
 }
 
-char* stepanov::getLine(std::istream& in)
+char* stepanov::getLine(std::istream& in, size_t& size)
 {
   const size_t step = 10;
-  size_t len = 0;
-  size_t mem_len = 10;
-  char* mem = nullptr;
-  try {
-    mem = new char[mem_len];
-  } catch (std::bad_alloc& e) {
-    return nullptr;
-  }
+  size_t capacity = 10;
+  char* mem = new char[capacity];
 
-  while (1) {
-    char c = in.get();
-    if (c == EOF || c == '\n') {
-      break;
-    }
-    if (len + 1 >= mem_len) {
-      mem_len += step;
+  char c = in.get();
+  while (!in.eof() && c != '\n') {
+    c = in.get();
+    if (size + 1 >= capacity) {
       char* new_mem = nullptr;
       try {
-        new_mem = new char[mem_len];
+        new_mem = new char[capacity + step];
       } catch (std::bad_alloc& e) {
         delete[] mem;
-        return nullptr;
+        throw e;
       }
-      for (size_t i = 0; i < len; i++) {
+      capacity += step;
+      for (size_t i = 0; i < size; i++) {
         new_mem[i] = mem[i];
       }
       delete[] mem;
       mem = new_mem;
     }
-    mem[len] = c;
-    len++;
+    mem[size] = c;
+    size++;
   }
-  mem[len] = '\0';
+  mem[size] = '\0';
   return mem;
 }
 
-void stepanov::toLower(char* str) {
+void stepanov::toLower(char* str)
+{
   for (size_t i = 0; str[i] != '\0'; i++) {
-    if (str[i] >= 'A' && str[i] <= 'Z') {
-      str[i] += 32;
+    if (std::isalpha(str[i])) {
+      str[i] = std::tolower(str[i]);
     }
   }
 }
 
 bool stepanov::isVowel(char c)
 {
-  switch (tolower(c)) {
+  switch (std::tolower(c)) {
     case 'a':
     case 'e':
     case 'i':
@@ -91,15 +89,15 @@ bool stepanov::isVowel(char c)
   }
 }
 
-void stepanov::removeVowels(char* str)
+void stepanov::removeVowels(const char* str, char* output)
 {
   size_t pos = 0;
   for (size_t i = 0; str[i] != '\0'; i++) {
     if (stepanov::isVowel(str[i])) {
       continue;
     }
-    str[pos] = str[i];
+    output[pos] = str[i];
     pos++;
   }
-  str[pos] = '\0';
+  output[pos] = '\0';
 }
