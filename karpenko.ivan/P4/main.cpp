eefec25e7190afbc0005f912605e564c4ab337b7
restorte <vanyakarpenko837@yandex.ru>
2025-12-13 22:11:25 +0300

    move myGetline to namespace

--- karpenko.ivan/P4/main.cpp
+++ karpenko.ivan/P4/main.cpp
@@ -7,14 +7,14 @@ namespace karpenko
   const size_t ALPHABET_SIZE = 26;
   const size_t ALPHABET_RESULT_SIZE = 27;
 
-  void uniTwo(const char * str1, const char * str2, char * result, size_t resultSize)
+  void uniTwo(const char *str1, const char *str2, char *result, size_t resultSize)
   {
     const size_t len1 = std::strlen(str1);
     const size_t len2 = std::strlen(str2);
     const size_t maxLen = (len1 > len2) ? len1 : len2;
 
     size_t resultIndex = 0;
-    
+
     for (size_t i = 0; i < maxLen; ++i)
     {
       if (i < len1 && resultIndex < resultSize - 1)
@@ -26,7 +26,7 @@ namespace karpenko
         result[resultIndex++] = str2[i];
       }
     }
-    
+
     if (len1 > len2)
     {
       for (size_t i = len2; i < len1 && resultIndex < resultSize - 1; ++i)
@@ -41,17 +41,17 @@ namespace karpenko
         result[resultIndex++] = str2[i];
       }
     }
-    
+
     result[resultIndex] = '\0';
   }
 
-  void shrSym(const char * input, char * result, size_t resultSize)
+  void shrSym(const char *input, char *result, size_t resultSize)
   {
     bool letters[ALPHABET_SIZE] = {false};
 
     for (size_t i = 0; input[i] != '\0'; ++i)
     {
-      const unsigned char uc = static_cast< unsigned char >(input[i]);
+      const unsigned char uc = static_cast<unsigned char>(input[i]);
       if (std::isalpha(uc))
       {
         const char lowerC = std::tolower(uc);
@@ -73,36 +73,35 @@ namespace karpenko
     }
     result[resultIndex] = '\0';
   }
-}
-
-bool myGetline(char * buffer, size_t bufferSize)
-{
-  if (bufferSize == 0)
+  bool myGetline(char *buffer, size_t bufferSize)
   {
-    return false;
-  }
-
-  size_t i = 0;
-  char c;
-
-  while (std::cin.get(c) && c != '\n')
-  {
-    if (i < bufferSize - 1)
+    if (bufferSize == 0)
     {
-      buffer[i++] = c;
-    }
-    else
-    {
-      while (std::cin.get(c) && c != '\n')
-      {
-      }
       return false;
     }
+
+    size_t i = 0;
+    char c;
+
+    while (std::cin.get(c) && c != '\n')
+    {
+      if (i < bufferSize - 1)
+      {
+        buffer[i++] = c;
+      }
+      else
+      {
+        while (std::cin.get(c) && c != '\n')
+        {
+        }
+        return false;
+      }
+    }
+
+    buffer[i] = '\0';
+
+    return !(std::cin.eof() && i == 0);
   }
-
-  buffer[i] = '\0';
-
-  return !(std::cin.eof() && i == 0);
 }
 
 int main()
@@ -118,8 +117,8 @@ int main()
 
   const char line2[] = "def_";
 
-  char * result1 = nullptr;
-  char * result2 = nullptr;
+  char *result1 = nullptr;
+  char *result2 = nullptr;
 
   try
   {
@@ -163,7 +162,7 @@ int main()
     std::cerr << "Error: cannot allocate memory for result\n";
     return 1;
   }
-  catch (const std::exception & e)
+  catch (const std::exception &e)
   {
     if (result1 != nullptr)
     {
