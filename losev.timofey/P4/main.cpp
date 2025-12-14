374e2c8afc71699e301b0764ef8f55f3bac72972
L0syash <losev_timofey07@mail.ru>
2025-12-14 19:28:33 +0300

    corrected the comments P4

--- losev.timofey/P4/main.cpp
+++ losev.timofey/P4/main.cpp
@@ -5,8 +5,8 @@
 
 namespace losev {
   char * getline(std::istream & in, size_t & size);
-  char * lat_rmv(const char * src, char * dest, size_t size);
-  int has_rep(const char * src, size_t size);
+  char * latRmv(const char * src, char * dest, size_t size);
+  int hasRep(const char * src, size_t size);
 }
 
 int main() {
@@ -17,41 +17,45 @@ int main() {
     std::cerr << "Fail memory\n";
     return 1;
   }
-  char * strForFirstEx = reinterpret_cast<char*>(malloc(str_size * sizeof(char)));
+  char * strForFirstEx = reinterpret_cast< char* >(malloc(str_size * sizeof(char)));
   if (!strForFirstEx) {
     std::cerr << "Memory allocation failed\n";
     free(str);
     return 1;
   }
-  for (size_t i = 0; i < str_size; i++) {
-    strForFirstEx[i] = '\0';
-  }
-  std::cout << los::lat_rmv(str, strForFirstEx, str_size) << "\n" << los::has_rep(str, str_size) << "\n";
+  std::cout << los::latRmv(str, strForFirstEx, str_size) << "\n" << los::hasRep(str, str_size) << "\n";
   free(str);
   free(strForFirstEx);
   return 0;
 }
 
-char * losev::getline(std::istream & in, size_t & size) {
-  char * str = reinterpret_cast<char*>(malloc(size * sizeof(char)));
+char * losev::getline(std::istream & in, size_t & capacity) {
+  capacity = 10;
+  char * str = reinterpret_cast< char* >(malloc(capacity * sizeof(char)));
   if (!str) {
-    std::cerr << "Memory allocation failed\n";
     return nullptr;
   }
   bool is_skips = in.flags() & std::ios_base::skipws;
   if (is_skips) {
     in >> std::noskipws;
   }
-  size_t i = 0;
-  for (; in && i < size; ++i) {
-    in >> str[i];
+  size_t size = 0;
+  for (; in && size < capacity; ++size) {
+    in >> str[size];
     if (in.fail()) {
-      break;
+      if (is_skips) {
+        in >> std::skipws;
+      }
+      free(str);
+      return nullptr;
     }
-    if (i == (size - 1)) {
-      char * str_new = reinterpret_cast<char*>(malloc((size + 10) * sizeof(char)));
+    if (size == (capacity - 1)) {
+      capacity += 10;
+      char * str_new = reinterpret_cast< char* >(malloc(capacity * sizeof(char)));
       if (!str_new) {
-        std::cerr << "Memory allocation failed\n";
+        if (is_skips) {
+          in >> std::skipws;
+        }
         free(str);
         return nullptr;
       }
@@ -59,57 +63,42 @@ char * losev::getline(std::istream & in, size_t & size) {
         str_new[j] = str[j];
       }
       free(str);
-      size += 10;
       str = str_new;
     }
   }
-  size = i + 1;
-  char * str_final = reinterpret_cast<char*>(malloc((size) * sizeof(char)));
-  if (!str_final) {
-    std::cerr << "Memory allocation failed\n";
-    free(str);
-    return nullptr;
-  }
-  for (size_t j = 0; j < size; j++) {
-    str_final[j] = str[j];
-  }
-  free(str);
-  str = str_final;
-  if (i == 0) {
+  if (size == 0) {
     free(str);
     if (is_skips) {
       in >> std::skipws;
     }
     return nullptr;
   }
-  str[size - 1] = '\0';
+  str[size] = '\0';
   if (is_skips) {
     in >> std::skipws;
   }
   return str;
 }
 
-char * losev::lat_rmv(const char * src, char * dest, size_t size) {
+char * losev::latRmv(const char * src, char * dest, size_t size) {
   size_t j = 0;
-  for (size_t i = 0; i < size && src[i] != '\0'; i++) {
+  for (size_t i = 0; src[i] != '\0' && j < size - 1; i++) {
     if (!isalpha(src[i])) {
-      if (j < size - 1) {
-        dest[j++] = src[i];
-      }
+      dest[j++] = src[i];
     }
   }
   dest[j] = '\0';
   return dest;
 }
 
-int losev::has_rep(const char * src, size_t size) {
+int losev::hasRep(const char * src, size_t size) {
   if (size < 2) {
     return 0;
   }
   const char * ptr = src + 1;
-  for (size_t i = 0; i < size - 1 && *src != '\0'; i++, src++) {
+  for (; *src != '\0'; src++) {
     ptr = src + 1;
-    for (size_t j = 0; j < size - 1 && *ptr != '\0'; j++, ptr++) {
+    for (; *ptr != '\0'; ptr++) {
       if (*ptr == *src) {
         return 1;
       }
