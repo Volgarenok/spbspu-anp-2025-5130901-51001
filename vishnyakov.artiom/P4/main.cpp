50a0ba0cfffd1c19bfddd58b20ab133e77c0516b
ArtConion <artiom0408@mail.ru>
2025-12-15 22:24:39 +0300

    add funtion for variant 7 (Amount_of_lat_letters)

--- vishnyakov.artiom/P4/main.cpp
+++ vishnyakov.artiom/P4/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 namespace vishnyakov
 {
+  size_t Amount_of_lat_letters(char *str, size_t size); // Вариант 7
+  void Uppercase_to_lowercase(char *str, size_t size); // Вариант 11
+  bool is_in(char *str, char letter, size_t size);
+}
+
+bool vishnyakov::is_in(char *str, char letter, size_t size)
+{
+  for(size_t i=0; i<size; ++i)
+  {
+    if(str[i] == letter)
+    {
+        return 1;
+    }
+  }
+  return 0;
+}
+
+
+size_t vishnyakov::Amount_of_lat_letters(char *str, size_t size)
+{
+    size_t result = 0;
+    const char *Lowercase_lat_alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const char *Uppercase_lat_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(size_t i=0; i<23;++i)
+    {
+      if(vishnyakov::is_in(str, Lowercase_lat_alphabet[i], size))
+      {
+        result += vishnyakov::is_in(str, Lowercase_lat_alphabet[i], size);
+      }
+      else
+      {
+        result += vishnyakov::is_in(str, Uppercase_lat_alphabet[i], size);
+      }
+    }
+    return result;
 }
 int main()
 {
