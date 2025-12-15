0161372cb0e5d4079238827da364980192b0081a
CWSHBR <mrcookie209@gmail.com>
2025-12-15 16:24:27 +0300

    Polygon ready

--- shaykhraziev.bulat/P5/main.cpp
+++ shaykhraziev.bulat/P5/main.cpp
@@ -37,6 +37,19 @@ namespace draw {
     explicit Rectangle(rectangle_t r);
   };
 
+  struct Polygon : Shape {
+    point_t * points;
+    size_t size;
+    point_t center;
+    double area;
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t to) override;
+    void move(double dx, double dy) override;
+    void scale(double coef) override;
+    explicit Polygon(point_t * a, size_t size);
+  };
+
   void scaleRelative(Shape& shp, point_t pt, double coef);
   rectangle_t getAllShapesFrameRect(Shape** shps, size_t size);
   void removeArray(Shape**shps, size_t size);
@@ -142,6 +155,77 @@ void draw::printParams(Shape** shps, size_t size) {
   }
   rectangle_t all = getAllShapesFrameRect(shps, size);
   std::cout << "(" << all.pos.x << ", " << all.pos.y << ") " << all.width << " " << all.height << "\n";
+}
+
+draw::Polygon::Polygon(point_t *a, size_t size): points(a), size(size), center({0, 0}){
+  if (size == 0) throw std::invalid_argument("Invalid size");
+  double x = 0, y = 0, S = 0;
+
+  if (size == 1) {
+    x = a[0].x;
+    y = a[0].y;
+  }
+  else if (size == 2) {
+    x = (a[0].x + a[1].x) / 2;
+    y = (a[0].y + a[1].y) / 2;
+  }
+  else {
+    for (size_t i = 0; i < size; i++) {
+      size_t j = i + 1, k = i - 1;
+      if (j == size) j = 0;
+      if (i == 0) k = size - 1;
+      S += a[i].x * (a[j].y - a[k].y);
+      x += (a[i].x + a[j].x) * (a[i].x * a[j].y - a[j].x * a[i].y);
+      y += (a[i].y + a[j].y) * (a[i].x * a[j].y - a[j].x * a[i].y);
+    }
+    S = abs(S) / 2.0;
+    x = x/(6*S);
+    y = y/(6*S);
+    area = S;
+  }
+
+  center = {x, y};
+}
+
+double draw::Polygon::getArea() const {
+  return area;
+}
+
+draw::rectangle_t draw::Polygon::getFrameRect() const {
+  double minx = points[0].x;
+  double miny = points[0].y;
+  double maxx = points[0].x;
+  double maxy = points[0].y;
+  for (size_t i = 1; i < size; i++) {
+    minx = std::min(minx, points[i].x);
+    miny = std::min(miny, points[i].y);
+    maxx = std::max(maxx, points[i].x);
+    maxy = std::max(maxy, points[i].y);
+  }
+  double width = maxx - minx, height = maxy - miny;
+  point_t p = {minx + width/2, miny + height/2};
+
+  return {width, height, p};
+}
+
+void draw::Polygon::move(double dx, double dy) {
+  for (size_t i = 0; i < size; i++) {
+    points[i].x += dx;
+    points[i].y += dy;
+  }
+}
+
+void draw::Polygon::move(point_t to) {
+  double dx = to.x - center.x, dy = to.y - center.y;
+  move(dx, dy);
+}
+
+void draw::Polygon::scale(double coef) {
+  area *= coef * coef;
+  for (size_t i = 0; i < size; ++i) {
+    points[i].x = center.x + (points[i].x - center.x) * coef;
+    points[i].y = center.y + (points[i].y - center.y) * coef;
+  }
 }
 
 int main() {
