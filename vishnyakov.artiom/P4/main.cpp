b66cc514c2e8dfced217d12adb1d9b1c23db7de7
ArtConion <artiom0408@mail.ru>
2025-12-15 23:26:13 +0300

    add function for variant 11 (Uppercase_to_Lowercase)

--- vishnyakov.artiom/P4/main.cpp
+++ vishnyakov.artiom/P4/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 namespace vishnyakov
 {
-  size_t Amount_of_lat_letters(char *str, size_t size); // Вариант 7
-  void Uppercase_to_lowercase(char *str, size_t size); // Вариант 11
-  bool is_in(char *str, char letter, size_t size);
+  size_t Amount_of_lat_letters(char *str); // Вариант 9
+  void Uppercase_to_lowercase(char *str); // Вариант 11
+  bool is_in(char *str, char letter);
 }
-
-bool vishnyakov::is_in(char *str, char letter, size_t size)
+bool vishnyakov::is_in(char *str, char letter)
 {
-  for(size_t i=0; i<size; ++i)
+  for(size_t i=0; str[i] != '\0'; ++i)
   {
     if(str[i] == letter)
     {
@@ -18,25 +17,40 @@ bool vishnyakov::is_in(char *str, char letter, size_t size)
   return 0;
 }
 
-
-size_t vishnyakov::Amount_of_lat_letters(char *str, size_t size)
+size_t vishnyakov::Amount_of_lat_letters(char *str)
 {
-    size_t result = 0;
-    const char *Lowercase_lat_alphabet = "abcdefghijklmnopqrstuvwxyz";
-    const char *Uppercase_lat_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for(size_t i=0; i<23;++i)
+  size_t result = 0;
+  const char *Lowercase_lat_alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const char *Uppercase_lat_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for(size_t i=0; i < 23; ++i)
+  {
+    if(vishnyakov::is_in(str, Lowercase_lat_alphabet[i]))
     {
-      if(vishnyakov::is_in(str, Lowercase_lat_alphabet[i], size))
-      {
-        result += vishnyakov::is_in(str, Lowercase_lat_alphabet[i], size);
-      }
-      else
-      {
-        result += vishnyakov::is_in(str, Uppercase_lat_alphabet[i], size);
-      }
+      result += vishnyakov::is_in(str, Lowercase_lat_alphabet[i]);
     }
-    return result;
+    else
+    {
+      result += vishnyakov::is_in(str, Uppercase_lat_alphabet[i]);
+    }
+  }
+  return result;
 }
+
+ void vishnyakov::Uppercase_to_lowercase(char *str)
+{
+  for(size_t i=0; str[i] != '\0'; i++)
+  {
+    if (std::isalpha(str[i]))
+    {
+        str[i] = std::tolower(str[i]);
+    }
+  }
+}
+
 int main()
 {
+    char *str = "asdaAasG";
+    std::cout << vishnyakov::Amount_of_lat_letters(str) << std::endl;
+    vishnyakov::Uppercase_to_lowercase(str);
+    std::cout << str;
 }
