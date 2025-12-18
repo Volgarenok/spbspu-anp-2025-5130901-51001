64d0339abe44997cea7bcb2c1f7baca18ad7557a
ArtConion <artiom0408@mail.ru>
2025-12-18 20:49:47 +0300

    add Point_t, Rectangle_t, Shape, Triangle and Concave structs

--- vishnyakov.artiom/P5/main.cpp
+++ vishnyakov.artiom/P5/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 namespace vishnyakov
 {
-  
+  struct point_t {
+    double x, y;
+  };
+
+  struct rectangle_t {
+    double width, height;
+    point_t pos;
+  };
+
+  struct Shape {
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void move(const point_t &newPos) = 0;
+    virtual void scale(double coefficient);
+    virtual ~Shape() = default;
+  };
+
+  struct Concave:Shape {
+    point_t spot1, spot2, spot3, spot4, center;
+    Concave(point_t a, point_t b, point_t c, point_t d);
+    double getArea() const override;
+    void move(double dx, double dy) override;
+    void move(const point_t &newPos) override;
+    void scale(double coefficient) override;
+  };
+
+  struct Triangle:Shape{
+    point_t spot1, spot2, spot3, center;
+    Triangle(point_t a, point_t b, point_t c);
+    double getArea() const override;
+    void move(double dx, double dy) override;
+    void move(const point_t &newPos) override;
+    void scale(double coefficient) override;
+  };
 }
 
 int main()
