de56a95114fb6bb91c6e34f72a49e52c1eea9b43
ahahhaha67 <krivoshapovewan@yandex.ru>
2025-12-18 20:54:11 +0300

    class:Shape

--- krivoshapov.ivan/P5/main.cpp
+++ krivoshapov.ivan/P5/main.cpp
@@ -14,4 +14,15 @@ namespace krivoshapov {
         point_t pos;
     };
 
+    class Shape {
+        public:
+          virtual ~Shape() = default;
+          virtual double getArea() const = 0;
+          virtual rectangle_t getFrameRect() const = 0;
+          virtual void move(double dx, double dy) = 0;
+          virtual void move(const point_t& newPos) = 0;
+          virtual void scale(double factor) = 0;
+    };
+
+
 }
