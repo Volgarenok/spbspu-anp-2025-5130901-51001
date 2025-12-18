4943e33634a606baa3f0df3aee9769811c4fc53b
ahahhaha67 <krivoshapovewan@yandex.ru>
2025-12-18 20:58:21 +0300

    class:Circle

--- krivoshapov.ivan/P5/main.cpp
+++ krivoshapov.ivan/P5/main.cpp
@@ -24,5 +24,19 @@ namespace krivoshapov {
           virtual void scale(double factor) = 0;
     };
 
+    class Circle : public Shape {
+        public:
+          Circle(const point_t& center, double radius);
+          double getArea() const override;
+          rectangle_t getFrameRect() const override;
+          void move(double dx, double dy) override;
+          void move(const point_t& newPos) override;
+          void scale(double factor) override;
+          point_t getCenter() const;
+
+        private:
+          point_t center_;
+          double radius_;
+    };
 
 }
