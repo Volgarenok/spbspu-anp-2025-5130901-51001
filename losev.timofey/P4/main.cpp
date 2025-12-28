f4c7c40e95fc163d912ba5ba34a7f63286474906
L0syash <losev_timofey07@mail.ru>
2025-12-28 22:11:57 +0300

    fix

--- losev.timofey/P4/main.cpp
+++ losev.timofey/P4/main.cpp
@@ -4,15 +4,16 @@
 #include<cctype>
 
 namespace losev {
-  char * getline(std::istream & in, size_t & size);
-  char * latRmv(const char * src, char * dest, size_t size);
-  int hasRep(const char * src, size_t size);
+  char * getline(std::istream & in, size_t & capacity, size_t & actual_size);
+  char * latRmv(const char * src, char * dest);
+  int hasRep(const char * src);
 }
 
 int main() {
   namespace los = losev;
   size_t str_size = 10;
-  char * str =  los::getline(std::cin, str_size);
+  size_t actual_len = 0;
+  char * str = los::getline(std::cin, str_size, actual_len);
   if (str == nullptr) {
     std::cerr << "Fail memory\n";
     return 1;
@@ -28,31 +29,28 @@ int main() {
     free(str);
     return 1;
   }
-  size_t actual_len = 0;
-  while (str[actual_len] != '\0') {
-    actual_len++;
-  }
-  std::cout << los::latRmv(str, strForFirstEx, str_size) << "\n" << los::hasRep(str, actual_len) << "\n";
+  strForFirstEx[0] = '\0';
+  std::cout << los::latRmv(str, strForFirstEx) << "\n" << los::hasRep(str) << "\n";
   free(str);
   free(strForFirstEx);
   return 0;
 }
 
-char * losev::getline(std::istream & in, size_t & capacity) {
+char * losev::getline(std::istream & in, size_t & capacity, size_t & actual_size) {
   capacity = 10;
   char * str = reinterpret_cast< char* >(malloc(capacity * sizeof(char)));
   if (!str) {
     return nullptr;
   }
+  actual_size = 0;
   bool is_skips = in.flags() & std::ios_base::skipws;
   if (is_skips) {
     in >> std::noskipws;
   }
-  size_t size = 0;
   while (in) {
-    if (size >= (capacity - 1)) {
-      capacity += 10;
-      char * str_new = reinterpret_cast< char* >(malloc(capacity * sizeof(char)));
+    if (actual_size >= (capacity - 1)) {
+      size_t new_capacity = capacity + 10;
+      char * str_new = reinterpret_cast< char* >(malloc(new_capacity * sizeof(char)));
       if (!str_new) {
         if (is_skips) {
           in >> std::skipws;
@@ -60,28 +58,29 @@ char * losev::getline(std::istream & in, size_t & capacity) {
         free(str);
         return nullptr;
       }
-      for (size_t j = 0; j < size; j++) {
+      for (size_t j = 0; j < actual_size; j++) {
         str_new[j] = str[j];
       }
       free(str);
       str = str_new;
+      capacity = new_capacity;
     }
-    in >> str[size];
+    in >> str[actual_size];
     if (in.fail()) {
       break;
     }
-    size++;
+    actual_size++;
   }
-  str[size] = '\0';
+  str[actual_size] = '\0';
   if (is_skips) {
     in >> std::skipws;
   }
   return str;
 }
 
-char * losev::latRmv(const char * src, char * dest, size_t size) {
+char * losev::latRmv(const char * src, char * dest) {
   size_t j = 0;
-  for (size_t i = 0; src[i] != '\0' && j < size - 1; i++) {
+  for (size_t i = 0; src[i] != '\0'; i++) {
     if (!isalpha(src[i])) {
       dest[j++] = src[i];
     }
@@ -90,10 +89,7 @@ char * losev::latRmv(const char * src, char * dest, size_t size) {
   return dest;
 }
 
-int losev::hasRep(const char * src, size_t size) {
-  if (size < 2) {
-    return 0;
-  }
+int losev::hasRep(const char * src) {
   const char * ptr = src + 1;
   for (; *src != '\0'; src++) {
     ptr = src + 1;
