cd56a7c8e046bb0085d307d6e314b02040447b52
SmirnovaMarya <unlenmaria@gmail.com>
2025-12-29 11:08:24 +0300

    struct Shape and class Rectangle

---
+++ smirnova.marya/P5/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+#include <cstddef>
+#include <stdexcept>
+namespace smirnova
+{
+  struct point_t {
+    double x, y;
+  };
+  struct rectangle_t {
+    double width, height;
+    point_t pos;
+  };
+  struct Shape
+  {
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(const point_t& p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+
+    void scale(double k) {
+      if (k <= 0.0) {
+        throw std::invalid_argument("Incorrect coefficient\n");
+      }
+      scaleImpl(k);
+    }
+    protected:
+      virtual void scaleImpl(double k) = 0;
+  };
+
+  class Rectangle final : public Shape {
+  private:
+    double width_{0.0}, height_{0.0};
+    point_t pos_{0.0, 0.0};
+  protected:
+    void scaleImpl(double k) override {
+      width_ *= k; 
+      height_ *= k;
+    }
+  public:
+    Rectangle() noexcept = default;
+
+    Rectangle(double width, double height, const point_t& pos) :
+    width_(width), height_(height), pos_(pos) 
+    {
+      if (width <= 0.0 || height <= 0.0) {
+        throw std::invalid_argument("Incorrect coordinates of a point\n");
+      }
+    }
+
+    double getArea() const noexcept override {
+      return width_ * height_;
+    }
+
+    rectangle_t getFrameRect() const noexcept override {
+      return {width_, height_, pos_};
+    }
+
+    void move(const point_t& p) noexcept override {
+      pos_ = p;
+    }
+
+    void move(double dx, double dy) noexcept override {
+      pos_.x += dx;
+      pos_.y += dy;
+    }
+  };
+}
