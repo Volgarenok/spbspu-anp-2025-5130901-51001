94c3c8d65d6539a4f604370d2445b1b332280cac
SmirnovaMarya <unlenmaria@gmail.com>
2025-12-30 23:53:22 +0300

    P5

--- smirnova.marya/P5/main.cpp
+++ smirnova.marya/P5/main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <stdexcept>
+#include <cmath>
 namespace smirnova
 {
   struct point_t {
@@ -11,6 +12,7 @@ namespace smirnova
     double width, height;
     point_t pos;
   };
+}
   struct Shape
   {
     virtual ~Shape() = default;
@@ -19,29 +21,14 @@ namespace smirnova
     virtual void move(const point_t& p) = 0;
     virtual void move(double dx, double dy) = 0;
     virtual void scale(double k) = 0;
-
-    void scale(double k) {
-      if (k <= 0.0) {
-        throw std::invalid_argument("Incorrect coefficient\n");
-      }
-      scaleImpl(k);
-    }
-    protected:
-      virtual void scaleImpl(double k) = 0;
   };
 
   class Rectangle final : public Shape {
   private:
     double width_{0.0}, height_{0.0};
     point_t pos_{0.0, 0.0};
-  protected:
-    void scaleImpl(double k) override {
-      width_ *= k; 
-      height_ *= k;
-    }
   public:
     Rectangle() noexcept = default;
-
     Rectangle(double width, double height, const point_t& pos) :
     width_(width), height_(height), pos_(pos)
     {
@@ -66,5 +53,50 @@ namespace smirnova
       pos_.x += dx;
       pos_.y += dy;
     }
+
+    void scale(double k) override {
+      if (k <= 0.0) {
+        throw std::invalid_argument("Incorrect coefficient\n");
+      }
+      width_ *= k; 
+      height_ *= k;
+    }
+  };
+  class Triangle final : public Shape {
+  private:
+    double base_{0.0}, height_{0.0};
+    point_t center_{0.0, 0.0};
+  protected:
+    void scaleImpl(double k) override {
+      base_ *= k;
+      height_ *= k;
+    }
+  public:
+    Triangle() noexcept = default;
+    Triangle(double base, double height, const point_t& center) :
+      base_(base), height_(height), center_(center)
+    {
+      if (base <= 0.0 || height <= 0.0) {
+        throw std::invalid_argument("Incorrect dimensions\n");
+      }
+    }
+
+    double getArea() const noexcept override {
+      return 0.5 * base_ * height_;
+    }
+
+    rectangle_t getFrameRect() const noexcept override {
+      return {base_, height_, center_};
+    }
+
+    void move(const point_t& p) noexcept override {
+      center_ = p;
+    }
+
+    void move(double dx, double dy) noexcept override {
+      center_.x += dx;
+      center_.y += dy;
+    }
   };
 }
+inline void validateTriangle(const smi)
