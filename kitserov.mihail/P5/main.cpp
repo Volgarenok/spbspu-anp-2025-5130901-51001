84da609cccb2ae6260e7d70e586d1ebe9ded08d9
Kreniker <kitserov.mike@gmail.com>
2026-01-05 16:56:40 +0300

    CG 1.4

--- kitserov.mihail/P5/main.cpp
+++ kitserov.mihail/P5/main.cpp
@@ -34,7 +34,7 @@ namespace kitserov
   private:
     rectangle_t rect_;
   };
-  
+
   struct Xquare final: Shape
   {
     Xquare(point_t p, float s);
@@ -47,7 +47,7 @@ namespace kitserov
     point_t centre_;
     float side_;
   };
-  
+
   struct Polygon final: Shape
   {
     Polygon(point_t* vertices, size_t vertexCount);
@@ -62,7 +62,7 @@ namespace kitserov
     size_t vertexCount_;
     point_t center_;
   };
-  
+
   rectangle_t frame(const point_t* pts, size_t s);
   void frameOutput(std::ostream& os, const rectangle_t& fr);
   void shapeOutput(std::ostream& os, const Shape* sh, const char* name);
@@ -77,7 +77,7 @@ int main()
   const size_t count = 3;
   Shape* shapes[count] = {};
   const char* shapeNames[] = {"Rectangle", "Xquare", "Polygon"};
-  
+
   try {
     shapes[0] = new Rectangle{{0.0, 0.0}, 1.0, 2.0};
     shapes[1] = new Xquare{{1.0, 0.0}, 2.0};
@@ -90,15 +90,15 @@ int main()
     }
     return 2;
   }
-  
+
   float totalAreaPrevious = 0.0;
   for (size_t i = 0; i < count; i++) {
     totalAreaPrevious += shapes[i]->getArea();
   }
-  
+
   std::cout << "BEFORE SCALING\n";
   printAllInfo(std::cout, shapes, shapeNames, count);
-  
+
   float k = 0;
   std::cout << "Ratio: ";
   std::cin >> k;
@@ -109,7 +109,7 @@ int main()
     }
     return 1;
   }
-  
+
   std::cout << "Point for scale:\n";
   float xx = 0.0;
   float yy = 0.0;
@@ -117,7 +117,7 @@ int main()
   std::cin >> xx;
   std::cout << "y = ";
   std::cin >> yy;
-  
+
   for (size_t i = 0; i < count; i++) {
     try {
       scalePoint(shapes[i], {xx, yy}, k);
@@ -129,10 +129,10 @@ int main()
       return 2;
     }
   }
-  
+
   std::cout << "AFTER SCALING\n";
   printAllInfo(std::cout, shapes, shapeNames, count);
-  
+
   for (size_t i = 0; i < count; i++) {
     delete shapes[i];
   }
@@ -179,19 +179,19 @@ kitserov::rectangle_t kitserov::frame(const point_t* pts, size_t s)
   float miny = pts[0].y;
   float maxx = pts[0].x;
   float maxy = pts[0].y;
-  
+
   for (size_t i = 0; i < s; ++i) {
     minx = std::min(minx, pts[i].x);
     miny = std::min(miny, pts[i].y);
     maxx = std::max(maxx, pts[i].x);
     maxy = std::max(maxy, pts[i].y);
   }
-  
+
   float width = maxx - minx;
   float height = maxy - miny;
   float centerX = minx + width / 2;
   float centerY = miny + height / 2;
-  
+
   return rectangle_t{width, height, {centerX, centerY}};
 }
 
@@ -201,7 +201,7 @@ void kitserov::frameOutput(std::ostream& os, const rectangle_t& fr)
   float top = fr.pos.y + fr.height / 2;
   float right = fr.pos.x + fr.width / 2;
   float bottom = fr.pos.y - fr.height / 2;
-  os << "Frame (leftTop " << left << ", " << top 
+  os << "Frame (leftTop " << left << ", " << top
      << " rightBot " << right << ", " << bottom << ")\n";
 }
 
@@ -264,11 +264,11 @@ kitserov::Polygon::Polygon(point_t* vertices, size_t vertexCount) :
     delete[] vertices_;
     throw std::logic_error("Polygon must have at least 3 vertices");
   }
-  
+
   for (size_t i = 0; i < vertexCount; ++i) {
     vertices_[i] = vertices[i];
   }
-  
+
   float sumX = 0;
   float sumY = 0;
   for (size_t i = 0; i < vertexCount_; ++i) {
@@ -298,24 +298,24 @@ kitserov::rectangle_t kitserov::Polygon::getFrameRect() const noexcept
   if (vertexCount_ == 0) {
     return {0, 0, {0, 0}};
   }
-  
+
   float minX = vertices_[0].x;
   float minY = vertices_[0].y;
   float maxX = vertices_[0].x;
   float maxY = vertices_[0].y;
-  
+
   for (size_t i = 1; i < vertexCount_; ++i) {
     minX = std::min(minX, vertices_[i].x);
     minY = std::min(minY, vertices_[i].y);
     maxX = std::max(maxX, vertices_[i].x);
     maxY = std::max(maxY, vertices_[i].y);
   }
-  
+
   float width = maxX - minX;
   float height = maxY - minY;
   float centerX = minX + width / 2;
   float centerY = minY + height / 2;
-  
+
   return {width, height, {centerX, centerY}};
 }
 
@@ -341,7 +341,7 @@ void kitserov::Polygon::scale(float k)
   if (k <= 0) {
     throw std::logic_error("bad ratio");
   }
-  
+
   for (size_t i = 0; i < vertexCount_; ++i) {
     vertices_[i].x = center_.x + (vertices_[i].x - center_.x) * k;
     vertices_[i].y = center_.y + (vertices_[i].y - center_.y) * k;
@@ -355,7 +355,7 @@ kitserov::rectangle_t kitserov::getOverallFrame(const Shape* const* shapes, size
   float maxX = firstFrame.pos.x + firstFrame.width / 2;
   float minY = firstFrame.pos.y - firstFrame.height / 2;
   float maxY = firstFrame.pos.y + firstFrame.height / 2;
-  
+
   for (size_t i = 1; i < count; ++i) {
     rectangle_t frame = shapes[i]->getFrameRect();
     float left = frame.pos.x - frame.width / 2;
@@ -367,12 +367,12 @@ kitserov::rectangle_t kitserov::getOverallFrame(const Shape* const* shapes, size
     minY = std::min(minY, bottom);
     maxY = std::max(maxY, top);
   }
-  
+
   float width = maxX - minX;
   float height = maxY - minY;
   float centerX = minX + width / 2;
   float centerY = minY + height / 2;
-  
+
   return {width, height, {centerX, centerY}};
 }
 
@@ -384,14 +384,14 @@ void kitserov::printAllInfo(std::ostream& os, const Shape* const* shapes, const 
       shapeOutput(os, shapes[i], name);
     }
   }
-  
+
   float totalArea = 0.0f;
   for (size_t i = 0; i < count; ++i) {
     if (shapes[i] != nullptr) {
       totalArea += shapes[i]->getArea();
     }
   }
-  
+
   os << "Total area = " << totalArea << "\n";
   rectangle_t overallFrame = getOverallFrame(shapes, count);
   os << "Overall frame:\n";
