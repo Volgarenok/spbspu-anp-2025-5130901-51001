0a51123eafd714488396ed4733f1daf2accaf36b
liia <kirilich_lubit@rambler.ru>
2026-01-15 01:24:50 +0300

    realise input string in main

--- nabieva.liia/P4/main.cpp
+++ nabieva.liia/P4/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <cstring>
 
 int main()
 {
+	size_t MAX_INPUT_SIZE = 1024;
+	size_t MAX_RESULT_SIZE = 2048;
+	char* input = new char[MAX_INPUT_SIZE];
+	if (!input) {
+		std::cerr << "can't give input memory" << std::endl;
+		return 1;
+	}
+	char* result = new char[MAX_RESULT_SIZE];
+	if (!result) {
+		std::cerr << "can't give result memory" << std::endl;
+		delete[] input;
+		return 1;
+	}
+	if (!std::cin.getline(input, MAX_INPUT_SIZE)) {
+		if (std::cin.fail()) {
+			std::cerr << "input error or too long string" << std::endl;
+			delete[] input;
+			delete[] result;
+			return 1;
+		}
+	}
+	for (size_t i = 0; i < strlen(input); i++) {
+		std::cout << input[i];
+	}
+	std::cout << "\n";
+
+
+	delete[] input;
+	delete[] result;
+	return 0;
 }
