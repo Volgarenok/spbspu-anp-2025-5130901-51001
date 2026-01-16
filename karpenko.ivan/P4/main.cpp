5db05d8b03ca293bb03f1b2dee3ea8b5cd0fb71d
restorte <vanyakarpenko837@yandex.ru>
2026-01-16 18:20:36 +0300

    add more new fucn for additional task

--- karpenko.ivan/P4/main.cpp
+++ karpenko.ivan/P4/main.cpp
@@ -57,6 +57,102 @@ namespace karpenko
     return buffer;
   }
 
+  char** readWords(std::istream& in, size_t& wordCount)
+  {
+    const size_t MAX_WORDS = 100;
+    char** words = new char*[MAX_WORDS];
+
+    wordCount = 0;
+    char c;
+    bool inWord = false;
+    size_t wordSize = 0;
+    char* currentWord = nullptr;
+
+    auto finishWord = [&]()
+    {
+      if (inWord && wordSize > 0)
+      {
+        if (wordCount >= MAX_WORDS)
+        {
+          for (size_t i = 0; i < wordCount; ++i)
+          {
+            delete[] words[i];
+          }
+          delete[] words;
+          std::cerr << "Error: too many words\n";
+          std::exit(1);
+        }
+        words[wordCount] = currentWord;
+        wordCount++;
+        inWord = false;
+        wordSize = 0;
+        currentWord = nullptr;
+      }
+    };
+
+    while (in.get(c))
+    {
+      if (c == '\n')
+      {
+        finishWord();
+        break;
+      }
+
+      if (isWordChar(c))
+      {
+        if (!inWord)
+        {
+          inWord = true;
+          currentWord = new char[INITIAL_CAPACITY];
+          wordSize = 0;
+        }
+
+        if (wordSize >= INITIAL_CAPACITY - 1)
+        {
+          size_t newCapacity = static_cast< size_t >(INITIAL_CAPACITY * GROW_FACTOR);
+          char* newBuffer = nullptr;
+          try
+          {
+            newBuffer = new char[newCapacity];
+          }
+          catch (const std::bad_alloc&)
+          {
+            delete[] currentWord;
+            throw;
+          }
+
+          std::memcpy(newBuffer, currentWord, wordSize);
+          delete[] currentWord;
+          currentWord = newBuffer;
+        }
+
+        currentWord[wordSize++] = c;
+        currentWord[wordSize] = '\0';
+      }
+      else
+      {
+        finishWord();
+      }
+    }
+
+    finishWord();
+
+    if (wordCount == 0)
+    {
+      delete[] words;
+      return nullptr;
+    }
+
+    char** resizedWords = new char*[wordCount];
+    for (size_t i = 0; i < wordCount; ++i)
+    {
+      resizedWords[i] = words[i];
+    }
+    delete[] words;
+
+    return resizedWords;
+  }
+
   void uniTwo(const char* str1, size_t len1, const char* str2, size_t len2,
     char* result, size_t resultSize)
   {
@@ -171,6 +267,31 @@ int main()
     karpenko::shrSym(line1, result2);
     std::cout << result2 << '\n';
 
+    size_t wordCount = 0;
+    std::cin.clear();
+    std::cin.seekg(0);
+
+    char** words = karpenko::readWords(std::cin, wordCount);
+
+    if (words != nullptr)
+    {
+      for (size_t i = 0; i < wordCount; ++i)
+      {
+        karpenko::uniTwo(words[i], std::strlen(words[i]), line2,
+          line2Length, result1, result1Size);
+        std::cout << result1 << '\n';
+
+        karpenko::shrSym(words[i], result2);
+        std::cout << result2 << '\n';
+      }
+
+      for (size_t i = 0; i < wordCount; ++i)
+      {
+        delete[] words[i];
+      }
+      delete[] words;
+    }
+
     delete[] line1;
     delete[] result1;
     delete[] result2;
@@ -181,6 +302,14 @@ int main()
     delete[] result1;
     delete[] result2;
     std::cerr << "Error: cannot allocate memory for results\n";
+    return 1;
+  }
+  catch (...)
+  {
+    delete[] line1;
+    delete[] result1;
+    delete[] result2;
+    std::cerr << "Error: unexpected exception\n";
     return 1;
   }
 
