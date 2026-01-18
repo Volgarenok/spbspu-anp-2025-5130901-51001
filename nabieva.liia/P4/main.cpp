85c91895e70dc7a5ef018ef2b9b32f1deea94026
liia <kirilich_lubit@rambler.ru>
2026-01-18 13:08:24 +0300

    realise func that form string with all letter

--- nabieva.liia/P4/main.cpp
+++ nabieva.liia/P4/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
+
+int formAllLetterString(char* input, char* result, size_t maxResultSize) {
+	unsigned char letters[26] = { 0 };
+	for (size_t i = 0; i < strlen(input); ++i) {
+		unsigned char c = static_cast<unsigned char>(input[i]);
+		if (std::isalpha(c)) {
+			c = std::tolower(c);
+			if (c >= 'a' && c <= 'z') {
+				letters[c - 'a'] = 1;
+			}
+		}
+	}
+	const char* secondString = "def_ghk";
+	for (size_t i = 0; i < strlen(secondString); ++i) {
+		unsigned char c = static_cast<unsigned char>(secondString[i]);
+		if (std::isalpha(c)) {
+			c = std::tolower(c);
+			if (c >= 'a' && c <= 'z') {
+				letters[c - 'a'] = 1;
+			}
+		}
+	}
+	size_t pos = 0;
+	for (int i = 0; i < 26; ++i) {
+		if (letters[i]) {
+			if (pos >= maxResultSize) {
+				return -1;
+			}
+			result[pos++] = static_cast<char>('a' + i);
+		}
+	}
+	return pos;
+}
 
 int main()
 {
@@ -7,28 +41,36 @@ int main()
 	size_t MAX_RESULT_SIZE = 2048;
 	char* input = new char[MAX_INPUT_SIZE];
 	if (!input) {
-		std::cerr << "can't give input memory" << std::endl;
+		std::cerr << "can't give input memory\n";
 		return 1;
 	}
 	char* result = new char[MAX_RESULT_SIZE];
 	if (!result) {
-		std::cerr << "can't give result memory" << std::endl;
+		std::cerr << "can't give result memory\n";
 		delete[] input;
 		return 1;
 	}
 	if (!std::cin.getline(input, MAX_INPUT_SIZE)) {
 		if (std::cin.fail()) {
-			std::cerr << "input error or too long string" << std::endl;
+			std::cerr << "input error or too long string\n";
 			delete[] input;
 			delete[] result;
 			return 1;
 		}
 	}
-	for (size_t i = 0; i < strlen(input); i++) {
-		std::cout << input[i];
+	int resultSize = formAllLetterString(input, result, MAX_RESULT_SIZE);
+	if (resultSize != -1) {
+		for (size_t i = 0; i < static_cast<size_t>(resultSize); i++) {
+			std::cout << result[i];
+		}
 	}
-	std::cout << "\n";
-
+	else {
+		std::cerr << "too long string\n";
+		delete[] input;
+		delete[] result;
+		return 1;
+	}
+	mergeString(input, result, MAX_RESULT_SIZE);
 
 	delete[] input;
 	delete[] result;
