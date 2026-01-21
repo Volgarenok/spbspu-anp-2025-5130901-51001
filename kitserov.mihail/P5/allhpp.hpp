1a2f3eeab4d3f0a87e59be9d7a0febe73ca39e4e
Kreniker <kitserov.mike@gmail.com>
2026-01-21 15:54:18 +0300

    allhpp hpp and main cpp

---
+++ kitserov.mihail/P5/allhpp.hpp
@@ -0,0 +1,10 @@
+#ifndef ALLHPP_HPP
+#define ALLHPP_HPP
+
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "xquare.hpp"
+#include "polygon.hpp"
+#include "utils.hpp"
+
+#endif
--- kitserov.mihail/P5/main.cpp
+++ kitserov.mihail/P5/main.cpp
@@ -1,3 +1,4 @@
+#include "allhpp.hpp"
 #include <iostream>
 
 int main()
