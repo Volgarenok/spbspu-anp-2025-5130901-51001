9667b6128222a64e9f62c138e1f956951d8253d4
a1dram <aldram54@gmail.com>
2026-01-21 05:45:46 +0300

    P4 or life

--- muraviev.alexander/P4/main.cpp
+++ muraviev.alexander/P4/main.cpp
@@ -24,7 +24,7 @@ char* muraviev::getline(std::istream& in, size_t& foundSize)
   bool is_skips = in.flags() & std::ios_base::skipws;
   size_t capacity = 16;
   size_t size = 0;
-  char* line = reinterpret_cast<char*>(malloc(sizeof(char) * capacity));
+  char* line = reinterpret_cast< char* >(malloc(sizeof(char) * capacity));
 
   if (!line) {
     foundSize = 0;
@@ -39,7 +39,7 @@ char* muraviev::getline(std::istream& in, size_t& foundSize)
   while (!(in >> sym).fail() && sym != '\n') {
     if (size + 1 >= capacity) {
       size_t new_capacity = capacity * 2;
-      char* tmp_line = reinterpret_cast<char*>(malloc(sizeof(char) * new_capacity));
+      char* tmp_line = reinterpret_cast< char* >(malloc(sizeof(char) * new_capacity));
 
       if (!tmp_line) {
         free(line);
@@ -66,7 +66,7 @@ char* muraviev::getline(std::istream& in, size_t& foundSize)
   if (size == 0 && !in) {
     free(line);
     foundSize = 0;
-    throw std::logic_error("Input failed");
+    return nullptr;
   }
 
   line[size] = '\0';
@@ -82,7 +82,8 @@ char* muraviev::intertwinedLine(char* tmp, const char* line1, const char* line2,
   size_t right = 0;
 
   for (size_t i = 0; i < bigSize; ++i) {
-    bool takeLeft;
+    bool takeLeft = false;
+
     if (left >= size_1) {
       takeLeft = false;
     } else if (right >= size_2) {
@@ -123,36 +124,36 @@ int main()
   size_t size_1 = 0;
   char* line1 = nullptr;
 
-  try {
-    line1 = muraviev::getline(std::cin, size_1);
+  line1 = muraviev::getline(std::cin, size_1);
 
-    if (line1 == nullptr) {
-      std::cerr << "Allocation error." << '\n';
-      return 1;
-    }
-  } catch (const std::logic_error& e) {
-    std::cerr << "Logic error: " << e.what() << '\n';
+  if (line1 == nullptr) {
+    std::cerr << "Error when getting a line." << '\n';
     return 1;
   }
 
   const char* line2 = "def ";
+
   size_t size_2 = std::strlen(line2);
-  char* inter_tmp = reinterpret_cast<char*>(malloc(sizeof(char) * (size_1 + size_2 + 1)));
+  size_t inter_size = size_1 + size_2;
+
+  char* inter_tmp = reinterpret_cast< char* >(malloc(sizeof(char) * (inter_size + 1)));
 
   if (!inter_tmp) {
     std::cerr << "Allocation error\n";
     free(line1);
     return 1;
   }
-  inter_tmp[0] = '\0';
+  inter_tmp[inter_size] = '\0';
 
   char* intertwined = muraviev::intertwinedLine(inter_tmp, line1, line2, size_1, size_2);
   std::cout << intertwined << "\n";
 
   const char* line3 = "g1h2k";
-  size_t size_3 = std::strlen(line3);
 
-  char* withDigs_tmp = reinterpret_cast<char*>(malloc(sizeof(char) * (size_1 + size_3 + 1)));
+  size_t size_3 = std::strlen(line3);
+  size_t withDigs_size = size_1 + size_3;
+
+  char* withDigs_tmp = reinterpret_cast< char* >(malloc(sizeof(char) * (withDigs_size + 1)));
 
   if (!withDigs_tmp) {
     std::cerr << "Allocation error\n";
@@ -160,7 +161,7 @@ int main()
     free(intertwined);
     return 1;
   }
-  withDigs_tmp[0] = '\0';
+  withDigs_tmp[withDigs_size] = '\0';
 
   char* lineWithDigs = muraviev::digitsAddedLine(withDigs_tmp, line1, line3, size_1);
   std::cout << lineWithDigs << "\n";
