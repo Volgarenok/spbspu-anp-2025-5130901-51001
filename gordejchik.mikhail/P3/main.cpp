21358d2fa59e23a5776a0fd11538eda7fcbb6d97
uncharted-bread <gordeychikmika@gmail.com>
2026-01-21 16:01:27 +0300

    added readMatrix

--- gordejchik.mikhail/P3/main.cpp
+++ gordejchik.mikhail/P3/main.cpp
@@ -1,2 +1,25 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <climits>
+
+namespace gordejchik
+{
+  bool readMatrix(std::ifstream& inputFile, int* matrix, size_t size) {
+    int num;
+    size_t c = 0;
+    while (inputFile >> num) {
+      matrix[c] = num;
+      c++;
+    }
+    if (inputFile.fail() && !inputFile.eof()) {
+      std::cerr << "Error input\0";
+      return false;
+    }
+    if (size != c) {
+      std::cerr << "Incorrect number of parameters\0";
+      return false;
+    }
+    return true;
+  }
+}
