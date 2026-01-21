14a4c2b730133ca33473e5b263d50f6206532e79
Kreniker <kitserov.mike@gmail.com>
2026-01-21 15:43:48 +0300

    xquare cpp

--- kitserov.mihail/P5/main.cpp
+++ kitserov.mihail/P5/main.cpp
@@ -60,36 +60,6 @@ int main()
   return 0;
 }
 
-kitserov::Xquare::Xquare(point_t p, float s) noexcept:
-  centre_(p),
-  side_(s)
-{}
-
-float kitserov::Xquare::getArea() const noexcept
-{
-  return 0.5 * side_ * side_;
-}
-
-kitserov::rectangle_t kitserov::Xquare::getFrameRect() const noexcept
-{
-  return {side_, side_, centre_};
-}
-
-void kitserov::Xquare::move(float dx, float dy) noexcept
-{
-  centre_.x += dx;
-  centre_.y += dy;
-}
-
-void kitserov::Xquare::move(const point_t& p) noexcept
-{
-  centre_ = p;
-}
-
-void kitserov::Xquare::scale(float k) noexcept
-{
-  side_ *= k;
-}
 
 kitserov::Polygon::Polygon(point_t* vertices, size_t vertexCount):
   vertices_(new point_t[vertexCount]),
