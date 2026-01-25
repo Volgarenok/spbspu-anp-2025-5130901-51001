4ab97d0d47a2c2a14b0776b9cc168fd9a62d5701
prblmtc11 <prblmtc11@gmail.com>
2026-01-25 23:12:08 +0300

    add command line arguments count validation

--- yarmolinskaya.anna/P3/main.cpp
+++ yarmolinskaya.anna/P3/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Invalid number of arguments\n";
+        return 1;
+    }
+
+    return 0;
 }
