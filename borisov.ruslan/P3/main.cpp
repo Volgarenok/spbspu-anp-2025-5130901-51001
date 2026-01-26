c8ea259c4aa3711810e52d820f53914a8e7b49d0
RBorisov6 <borisovruslan1512@gmail.com>
2026-01-26 21:17:10 +0300

    delete excess else-if

--- borisov.ruslan/P3/main.cpp
+++ borisov.ruslan/P3/main.cpp
@@ -73,9 +73,6 @@ int main(int argc, char ** argv)
   if (argc < 4) {
     std::cerr << "Not enough arguments\n";
     return 1;
-  } else if (argc > 4) {
-    std::cerr << "Too many arguments\n";
-    return 1;
   }
 
   char* endptr;
