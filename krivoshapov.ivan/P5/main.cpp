28a45aa8f9b6a2303168fa9586f33c3ca2305e55
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-02-05 21:56:01 +0300

    f2 fix

--- krivoshapov.ivan/P5/main.cpp
+++ krivoshapov.ivan/P5/main.cpp
@@ -33,7 +33,7 @@ namespace krivoshapov
     virtual void scaleUnchecked(double factor) noexcept = 0;
   };
 
-  class Circle final : public Shape
+  class Circle final: public Shape
   {
   public:
     Circle(const point_t& center, double radius);
@@ -49,7 +49,7 @@ namespace krivoshapov
     double radius_;
   };
 
-  class Rectangle final : public Shape
+  class Rectangle final: public Shape
   {
   public:
     Rectangle(const point_t& center, double width, double height);
@@ -66,7 +66,7 @@ namespace krivoshapov
     double height_;
   };
 
-  class Rubber final : public Shape
+  class Rubber final: public Shape
   {
   public:
     Rubber(const point_t& circleCenter, double radius, const point_t& shapeCenter);
@@ -92,7 +92,7 @@ namespace krivoshapov
     scaleUnchecked(factor);
   }
 
-  Circle::Circle(const point_t& center, double radius) :
+  Circle::Circle(const point_t& center, double radius):
     center_(center),
     radius_(radius)
   {
